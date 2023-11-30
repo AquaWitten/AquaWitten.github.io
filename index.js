d940766f80638e0c0e5a2e1b7b37bafd5503f69c
Emil Wittendorff <emilwittendorff@hotmail.com>
2023-11-30 20:58:30 +0100

    Added initial offline implementation

---
+++ index.js
@@ -0,0 +1,94 @@
+const jsonTestData =
+  '{"event":"2020","members":{"1048865":{"completion_day_level":{"1":{"1":{},"2":{"get_star_ts":1606938057}},"2":{"1":{"get_star_ts":1606939262},"2":{"get_star_ts":1606939793}},"3":{"1":{"get_star_ts":1606985689},"2":{"get_star_ts":1606985925}},"4":{"1":{"get_star_ts":1607664715},"2":{"get_star_ts":1607667073}},"5":{"1":{"get_star_ts":1607669884},"2":{"get_star_ts":1607670134}},"6":{"1":{"get_star_ts":1607710118},"2":{"get_star_ts":1607718262}},"7":{"1":{"get_star_ts":1607789531},"2":{"get_star_ts":1607793938}}},"global_score":0,"id":"1048865","last_star_ts":1607793938,"local_score":24,"name":"Michael Bieniek","stars":14},"307156":{"completion_day_level":{"1":{"1":{"get_star_ts":1606839016},"2":{"get_star_ts":1606839284}},"10":{"1":{"get_star_ts":1607610793},"2":{"get_star_ts":1607611286}},"11":{"1":{"get_star_ts":1607664253},"2":{"get_star_ts":1607665085}},"12":{"1":{"get_star_ts":1607749769},"2":{"get_star_ts":1607751890}},"13":{"1":{"get_star_ts":1607835946},"2":{"get_star_ts":1607840304}},"14":{"1":{"get_star_ts":1607957764},"2":{"get_star_ts":1607961552}},"15":{"1":{"get_star_ts":1608009632},"2":{"get_star_ts":1608009699}},"16":{"1":{"get_star_ts":1608095491},"2":{"get_star_ts":1608096970}},"17":{"1":{"get_star_ts":1608219896},"2":{"get_star_ts":1608229409}},"18":{"1":{"get_star_ts":1608304671},"2":{"get_star_ts":1608305127}},"19":{"1":{"get_star_ts":1608355949},"2":{"get_star_ts":1608359869}},"2":{"1":{"get_star_ts":1606922530},"2":{"get_star_ts":1606922727}},"20":{"1":{"get_star_ts":1608522674},"2":{"get_star_ts":1608530458}},"21":{"1":{"get_star_ts":1608561917},"2":{"get_star_ts":1608562242}},"22":{"1":{"get_star_ts":1608613738},"2":{"get_star_ts":1608615791}},"23":{"1":{"get_star_ts":1608704918},"2":{"get_star_ts":1608707867}},"24":{"1":{"get_star_ts":1608786558},"2":{"get_star_ts":1608787503}},"25":{"1":{"get_star_ts":1609010169},"2":{"get_star_ts":1609010186}},"3":{"1":{"get_star_ts":1606971862},"2":{"get_star_ts":1606972053}},"4":{"1":{"get_star_ts":1607058503},"2":{"get_star_ts":1607059557}},"5":{"1":{"get_star_ts":1607230415},"2":{"get_star_ts":1607230701}},"6":{"1":{"get_star_ts":1607230999},"2":{"get_star_ts":1607231455}},"7":{"1":{"get_star_ts":1607353193},"2":{"get_star_ts":1607354414}},"8":{"1":{"get_star_ts":1607403913},"2":{"get_star_ts":1607404451}},"9":{"1":{"get_star_ts":1607490327},"2":{"get_star_ts":1607491067}}},"global_score":0,"id":"307156","last_star_ts":1609010186,"local_score":263,"name":"Orhan Can \u00D6zalp","stars":50},"682929":{"completion_day_level":{"1":{"1":{"get_star_ts":1606799525},"2":{"get_star_ts":1606799808}},"10":{"1":{"get_star_ts":1607611050},"2":{"get_star_ts":1607615630}},"11":{"1":{"get_star_ts":1607698925},"2":{"get_star_ts":1607701327}},"12":{"1":{"get_star_ts":1607750354},"2":{"get_star_ts":1607753069}},"13":{"1":{"get_star_ts":1607870435},"2":{"get_star_ts":1607894835}},"14":{"1":{"get_star_ts":1607958175},"2":{"get_star_ts":1607962398}},"15":{"1":{"get_star_ts":1608045636},"2":{"get_star_ts":1608045750}},"16":{"1":{"get_star_ts":1608130405},"2":{"get_star_ts":1608134928}},"17":{"1":{"get_star_ts":1608219758},"2":{"get_star_ts":1608228383}},"18":{"1":{"get_star_ts":1608306935},"2":{"get_star_ts":1608312155}},"19":{"1":{"get_star_ts":1608358583},"2":{"get_star_ts":1608365503}},"2":{"1":{"get_star_ts":1606885738},"2":{"get_star_ts":1606885942}},"20":{"1":{"get_star_ts":1608489578},"2":{"get_star_ts":1608499708}},"21":{"1":{"get_star_ts":1608569730},"2":{"get_star_ts":1608596126}},"22":{"1":{"get_star_ts":1608648221},"2":{"get_star_ts":1608652527}},"23":{"1":{"get_star_ts":1608737316},"2":{"get_star_ts":1608758348}},"24":{"1":{"get_star_ts":1608823512},"2":{"get_star_ts":1608825193}},"25":{"1":{"get_star_ts":1609010838},"2":{"get_star_ts":1609010847}},"3":{"1":{"get_star_ts":1606971894},"2":{"get_star_ts":1606972113}},"4":{"1":{"get_star_ts":1607058830},"2":{"get_star_ts":1607059569}},"5":{"1":{"get_star_ts":1607179910},"2":{"get_star_ts":1607180411}},"6":{"1":{"get_star_ts":1607265981},"2":{"get_star_ts":1607266576}},"7":{"1":{"get_star_ts":1607352746},"2":{"get_star_ts":1607356227}},"8":{"1":{"get_star_ts":1607438494},"2":{"get_star_ts":1607438937}},"9":{"1":{"get_star_ts":1607524548},"2":{"get_star_ts":1607525224}}},"global_score":0,"id":"682929","last_star_ts":1609010847,"local_score":209,"name":"Chris Thomas","stars":50},"775842":{"completion_day_level":{},"global_score":0,"id":"775842","last_star_ts":"0","local_score":0,"name":"Jacob Tsafatinos","stars":0},"964504":{"completion_day_level":{"1":{"1":{"get_star_ts":1606834617},"2":{"get_star_ts":1606834869}},"10":{"1":{"get_star_ts":1607611662},"2":{"get_star_ts":1607612670}},"11":{"1":{"get_star_ts":1607698714},"2":{"get_star_ts":1607701691}},"12":{"1":{"get_star_ts":1607785741},"2":{"get_star_ts":1607788184}},"13":{"1":{"get_star_ts":1607870494},"2":{"get_star_ts":1607879639}},"14":{"1":{"get_star_ts":1607957601},"2":{"get_star_ts":1607959652}},"15":{"1":{"get_star_ts":1608043849},"2":{"get_star_ts":1608044675}},"16":{"1":{"get_star_ts":1608130699},"2":{"get_star_ts":1608133281}},"17":{"1":{"get_star_ts":1608960526},"2":{"get_star_ts":1608962563}},"18":{"1":{"get_star_ts":1608306636},"2":{"get_star_ts":1608307136}},"19":{"1":{"get_star_ts":1608360469},"2":{"get_star_ts":1608393691}},"2":{"1":{"get_star_ts":1606890208},"2":{"get_star_ts":1606890786}},"20":{"1":{"get_star_ts":1608488193},"2":{"get_star_ts":1608532398}},"21":{"1":{"get_star_ts":1608566647},"2":{"get_star_ts":1608567195}},"22":{"1":{"get_star_ts":1608648051},"2":{"get_star_ts":1608653778}},"23":{"1":{"get_star_ts":1608742999},"2":{"get_star_ts":1608767562}},"24":{"1":{"get_star_ts":1608823759},"2":{"get_star_ts":1608825501}},"25":{"1":{"get_star_ts":1609010404},"2":{"get_star_ts":1609010410}},"3":{"1":{"get_star_ts":1606972790},"2":{"get_star_ts":1606973336}},"4":{"1":{"get_star_ts":1607091987},"2":{"get_star_ts":1607100278}},"5":{"1":{"get_star_ts":1607180134},"2":{"get_star_ts":1607180425}},"6":{"1":{"get_star_ts":1607265615},"2":{"get_star_ts":1607266465}},"7":{"1":{"get_star_ts":1607353867},"2":{"get_star_ts":1607354958}},"8":{"1":{"get_star_ts":1607438787},"2":{"get_star_ts":1607440481}},"9":{"1":{"get_star_ts":1607524924},"2":{"get_star_ts":1607525383}}},"global_score":0,"id":"964504","last_star_ts":1609010410,"local_score":191,"name":"S. Sepehr","stars":50},"989139":{"completion_day_level":{"1":{"1":{"get_star_ts":1606800820},"2":{"get_star_ts":1606800911}},"10":{"1":{"get_star_ts":1607611161},"2":{"get_star_ts":1607614680}},"11":{"1":{"get_star_ts":1607699126},"2":{"get_star_ts":1607699659}},"12":{"1":{"get_star_ts":1607785393},"2":{"get_star_ts":1607786857}},"13":{"1":{"get_star_ts":1607870565},"2":{"get_star_ts":1608059415}},"14":{"1":{"get_star_ts":1607977812},"2":{"get_star_ts":1607981395}},"15":{"1":{"get_star_ts":1608044261},"2":{"get_star_ts":1608044901}},"16":{"1":{"get_star_ts":1608129783},"2":{"get_star_ts":1608135136}},"17":{"1":{"get_star_ts":1608219251},"2":{"get_star_ts":1608221267}},"18":{"1":{"get_star_ts":1608303914},"2":{"get_star_ts":1608307587}},"19":{"1":{"get_star_ts":1608389647},"2":{"get_star_ts":1608392931}},"2":{"1":{"get_star_ts":1606885974},"2":{"get_star_ts":1606886197}},"20":{"1":{"get_star_ts":1608476879}},"21":{"1":{"get_star_ts":1608561936},"2":{"get_star_ts":1608564089}},"22":{"1":{"get_star_ts":1608614289},"2":{"get_star_ts":1608998199}},"23":{"1":{"get_star_ts":1608786497},"2":{"get_star_ts":1608836377}},"24":{"1":{"get_star_ts":1608821475},"2":{"get_star_ts":1608823318}},"25":{"1":{"get_star_ts":1609010773}},"3":{"1":{"get_star_ts":1606972001},"2":{"get_star_ts":1606972476}},"4":{"1":{"get_star_ts":1607058411},"2":{"get_star_ts":1607096009}},"5":{"1":{"get_star_ts":1607180199},"2":{"get_star_ts":1607180509}},"6":{"1":{"get_star_ts":1607265365},"2":{"get_star_ts":1607265551}},"7":{"1":{"get_star_ts":1607353470},"2":{"get_star_ts":1607354454}},"8":{"1":{"get_star_ts":1607438242},"2":{"get_star_ts":1607443386}},"9":{"1":{"get_star_ts":1607524874},"2":{"get_star_ts":1607526818}}},"global_score":0,"id":"989139","last_star_ts":1609010773,"local_score":195,"name":"Stefan Gavrilovic","stars":48}},"owner_id":"682929"}';
+var participants = [];
+
+window.onload = () => {
+  processJsonData(jsonTestData);
+  //makeScoreChart();
+  makeStarScoreChart();
+};
+
+function processJsonData(jsonStr) {
+  data = JSON.parse(jsonStr);
+  for (let memberId in data.members) {
+    let member = data.members[memberId];
+    participants.push({
+      name: member.name ? member.name : memberId,
+      stars: member.stars,
+      score: member.local_score,
+      starDates: starDatesToArray(member.completion_day_level),
+    });
+  }
+  console.log(participants);
+}
+
+function starDatesToArray(starsObj) {
+  return Object.entries(starsObj).map(([day, stars]) => {
+    return {
+      day,
+      stars: Object.values(stars).filter((star) => star.get_star_ts).length,
+    };
+  });
+}
+
+//Local score
+function makeScoreChart() {
+  new Chart(document.getElementById("starChart"), {
+    type: "bar",
+    data: {
+      labels: participants.map((participan) => participan.name),
+      datasets: [
+        {
+          label: "Acquisitions by year",
+          data: participants.map((participan) => participan.score),
+        },
+      ],
+    },
+  });
+}
+
+//Star score
+function makeStarScoreChart() {
+  new Chart(document.getElementById("starChart"), {
+    type: "bar",
+    data: {
+      labels: participants.map((participan) => participan.name),
+      datasets: [
+        {
+          label: "Acquisitions by year",
+          data: participants.map((participan) => participan.stars),
+          backgroundColor: [
+            "rgba(17, 54, 34, 1)",
+            "rgba(69, 103, 89, 1)",
+            "rgba(235, 161, 90, 1)",
+            "rgba(203, 52, 87, 1)",
+            "rgba(134, 2, 42, 1)",
+            "rgba(89, 0, 18, 1)",
+            "rgba(26, 58, 9, 1)",
+            "rgba(77, 126, 43, 1)",
+            "rgba(175, 212, 143, 1)",
+            "rgba(230, 170, 206, 1)",
+            "rgba(179, 18, 80, 1)",
+            "rgba(99, 23, 59, 1)",
+            "rgba(91, 206, 15, 1)",
+            "rgba(245, 144, 160, 1)",
+            "rgba(64, 80, 105, 1)",
+            "rgba(218, 33, 39, 1)",
+            "rgba(198, 161, 28, 1)",
+          ],
+        },
+      ],
+    },
+  });
+}
+
+//Potential stars vs actual stars per day
+
+function fetchJsonData() {
+  fetch("testdata.json")
+    .then((response) => response.json)
+    .then((json) => {
+      console.log(JSON.stringify(json));
+      adventData = json;
+    });
+}
